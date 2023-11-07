0361898d58849967060544bb16b874511e56e901
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2023-11-07 12:25:21 +0700

    update script get post data

--- Script/data_pq_to_thingsboard_script.py
+++ Script/data_pq_to_thingsboard_script.py
@@ -1,6 +1,26 @@
 import json
 import requests
 import psycopg2
+import time
+import pandas as pd
+
+
+def calculateTimestamp(timeseries):
+
+    # Create a sample timestamp
+
+    # Convert timestamp to datetime object
+    datetime_object = pd.to_datetime(timeseries, unit='ms')
+
+    # Convert datetime object to date
+    return datetime_object
+
+
+def calculateTimeseries(dateTime):
+   return int(dateTime.timestamp()*1000)
+
+
+
 
 def post_data_to_api(data, api_url, auth_token):
     headers = {
@@ -12,7 +32,9 @@ def post_data_to_api(data, api_url, auth_token):
     response = requests.post(api_url, headers=headers, data=json.dumps(data))
 
     if response.status_code != 200:
-        raise Exception(f"Failed to post data to API: {response.status_code} {response.content}")
+        raise Exception(
+            f"Failed to post data to API: {response.status_code} {response.content}")
+
 
 if __name__ == "__main__":
     # Connect to the PostgreSQL database
@@ -28,31 +50,26 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         """
-        SELECT * FROM "weatherData";
+            SELECT Date("dateTime") as day, avg("outsideTemp") as average_temp
+            FROM "weatherData"
+            GROUP BY DATE("dateTime");
         """
     )
 
-    
     data = []
     for row in cur:
         data.append(
             {
-                "ts": 1699260201000,  
+                "ts": int(calculateTimeseries(pd.to_datetime(row[0]))),
                 "values": {
-                    "outsideHumidity": row[0],
-                    "outsideMaxTemp": row[1],
-                    "outsideMinTemp": row[2],
-                    "outsideTemp": row[3]
+                    "avgTemp" : row[1]
                 },
             }
         )
-
+    print(data)
     # Format the results of your query as JSON
-    json_data = json.dumps(data)
-
-    print(json_data)
     # Post the JSON data to the API
-    api_url = "http://localhost:8080/api/plugins/telemetry/ASSET/5e66af30-7c80-11ee-bc01-716cbc946983/timeseries/ANY?scope=ANY"
-    auth_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMGY3NmRmZDAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiIxZTVhOWUxZS0xOTg1LTRkZDctYTg5ZS03ZGY3ZWYxNzMwN2IiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTY5OTI1ODY0MSwiZXhwIjoxNjk5MjY3NjQxLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMGYyNjc1ZTAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.hkATK--56Tb1C92A4tTGq2fjpK1mel2_-D-K9anfG6G9tv9VZsHOuoTZvJIYHaHFQclUwwnwf_Kade42-34vfw"
+    api_url = "http://localhost:8080/api/plugins/telemetry/ASSET/99643c50-7731-11ee-b094-a797de75d9ec/timeseries/ANY?scope=ANY"
+    auth_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMGY3NmRmZDAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiIxZTVhOWUxZS0xOTg1LTRkZDctYTg5ZS03ZGY3ZWYxNzMwN2IiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTY5OTMyNjc0NSwiZXhwIjoxNjk5MzM1NzQ1LCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMGYyNjc1ZTAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.uXByHUYVSxBSY9eiWYkRSvKiRMxI_xknF7BjaEcnme3qGF6M0yVAcyjtgI1iM_4_5i8HxNDh94A6cBJYzBdTPg"
 
     post_data_to_api(data, api_url, auth_token)
--- Script/data_to_pg_script.py
+++ Script/data_to_pg_script.py
@@ -6,13 +6,31 @@ import requests
 import json
 import psycopg2
 
+import pandas as pd
+
+def calculateTimestamp(timeseries):
+
+    # Create a sample timestamp
+
+
+    # Convert timestamp to datetime object
+    datetime_object = pd.to_datetime(timeseries, unit='ms')
+
+    # Convert datetime object to date
+    return datetime_object
+
+startTs = 0
+endTs = 1799315200000
+
+print(calculateTimestamp(startTs))
 # API
 url = "http://localhost:8080/api/plugins/telemetry/ASSET/99643c50-7731-11ee-b094-a797de75d9ec/values/timeseries"
-querystring = {"keys":"outsideHumidity,outsideMaxTemp,outsideMinTemp,outsideTemp","startTs":"1699002500000","endTs":"1699002520000"}
+querystring = {"keys": "outsideHumidity,outsideMaxTemp,outsideMinTemp,outsideTemp",
+               "startTs": startTs, "endTs": endTs}
 
 headers = {
     "accept": "application/json",
-    "X-Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMGY3NmRmZDAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiIxZTVhOWUxZS0xOTg1LTRkZDctYTg5ZS03ZGY3ZWYxNzMwN2IiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTY5OTExNTMxMywiZXhwIjoxNjk5MTI0MzEzLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMGYyNjc1ZTAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.9HQrcGCuNpo3g53TPaV14C9-SMl-h6YGqIWpUt8F_hwu15Rs1fwMOw9ZhTuRAYUFFqxppVSg0ieElUt0n5sg4Q"
+    "X-Authorization": "Bearer  eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiMGY3NmRmZDAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiIxZTVhOWUxZS0xOTg1LTRkZDctYTg5ZS03ZGY3ZWYxNzMwN2IiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTY5OTMyNjc0NSwiZXhwIjoxNjk5MzM1NzQ1LCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMGYyNjc1ZTAtNmU0ZC0xMWVlLTk5YjUtZmY0MGI1ZTk4MjUxIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.uXByHUYVSxBSY9eiWYkRSvKiRMxI_xknF7BjaEcnme3qGF6M0yVAcyjtgI1iM_4_5i8HxNDh94A6cBJYzBdTPg"
 }
 
 # Get data from API, return json string
@@ -20,9 +38,10 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 
 # Convert json string to dict
 data = json.loads(response.text)
-
+        
 # Initial database
-conn = psycopg2.connect(database="weather", user="postgres", password="postgres", host="localhost", port="5432")
+conn = psycopg2.connect(database="weather", user="postgres",
+                        password="postgres", host="localhost", port="5432")
 cur = conn.cursor()
 
 # Insert the data into the PostgreSQL table
@@ -34,13 +53,12 @@ outsideTempArr = data['outsideTemp']
 
 
 for i in range(len(data['outsideHumidity'])):
-    
-    cur.execute('INSERT INTO "weatherData"("outsideHumidity", "outsideMaxTemp", "outsideMinTemp", "outsideTemp") VALUES (%s,%s,%s,%s)', (outsideHumidityArr[i]['value'], outsideMaxTempArr[i]['value'], outsideMinTempArr[i]['value'], outsideTempArr[i]['value']))
-    
+    print(calculateTimestamp(outsideHumidityArr[i]['ts']))
+    cur.execute('INSERT INTO "weatherData"("dateTime","outsideHumidity", "outsideMaxTemp", "outsideMinTemp", "outsideTemp") VALUES (%s,%s,%s,%s,%s)',
+                (calculateTimestamp(outsideHumidityArr[i]['ts']),outsideHumidityArr[i]['value'], outsideMaxTempArr[i]['value'], outsideMinTempArr[i]['value'], outsideTempArr[i]['value']))
+
 
 # Commit the changes and close the connection
 conn.commit()
 cur.close()
 conn.close()
-
-
